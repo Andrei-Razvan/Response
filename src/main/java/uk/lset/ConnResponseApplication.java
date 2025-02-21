055b17f0c3b960a490e8bda6999cd29551ebbefd
ap0l0d0r <camenicandreirazvan@yahoo.com>
2025-02-21 10:54:21 +0000

    first commit

---
+++ src/main/java/uk/lset/ConnResponseApplication.java
@@ -0,0 +1,13 @@
+package uk.lset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnResponseApplication.class, args);
+	}
+
+}
